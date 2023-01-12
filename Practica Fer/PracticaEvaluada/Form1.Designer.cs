a48056ce8dd5f14df41cc2c96e02cbe6872dd700
Oscar Cortez <48421554+OscarACortez@users.noreply.github.com>
2023-01-12 12:38:53 -0600

    Add files via upload

---
+++ Practica Fer/PracticaEvaluada/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace PracticaEvaluada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtJ = new System.Windows.Forms.TextBox();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvJuegos = new System.Windows.Forms.DataGridView();
+            this.NJ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Jugador = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJuegos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(480, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formulario para Ingresar Informacion De Juegos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre del Juego :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(71, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jugador :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label4.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(71, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Precio :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cantidad :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label6.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(40, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(220, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ingrese ID para Buscar :";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(382, 55);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 6;
+            this.txtN.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtJ
+            // 
+            this.txtJ.Location = new System.Drawing.Point(382, 103);
+            this.txtJ.Name = "txtJ";
+            this.txtJ.Size = new System.Drawing.Size(100, 20);
+            this.txtJ.TabIndex = 7;
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(382, 158);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(100, 20);
+            this.txtP.TabIndex = 8;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(382, 212);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 9;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(382, 267);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 10;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnGuardar.Location = new System.Drawing.Point(44, 365);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(80, 28);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnBuscar.Location = new System.Drawing.Point(177, 365);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(80, 28);
+            this.btnBuscar.TabIndex = 12;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnModificar.Location = new System.Drawing.Point(317, 365);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(80, 28);
+            this.btnModificar.TabIndex = 13;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnEliminar.Location = new System.Drawing.Point(445, 365);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(80, 28);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvJuegos
+            // 
+            this.dgvJuegos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJuegos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NJ,
+            this.Jugador,
+            this.Precio,
+            this.Cantidad});
+            this.dgvJuegos.Location = new System.Drawing.Point(579, 52);
+            this.dgvJuegos.Name = "dgvJuegos";
+            this.dgvJuegos.Size = new System.Drawing.Size(544, 294);
+            this.dgvJuegos.TabIndex = 15;
+            this.dgvJuegos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvJuegos_CellContentClick);
+            // 
+            // NJ
+            // 
+            this.NJ.HeaderText = "Nombre del Jugador";
+            this.NJ.Name = "NJ";
+            // 
+            // Jugador
+            // 
+            this.Jugador.HeaderText = "Jugador";
+            this.Jugador.Name = "Jugador";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.ForeColor = System.Drawing.Color.White;
+            this.btnSalir.Location = new System.Drawing.Point(1083, 395);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(85, 28);
+            this.btnSalir.TabIndex = 16;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1170, 422);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dgvJuegos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.txtJ);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJuegos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtJ;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvJuegos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NJ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Jugador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Practica Fer/PracticaEvaluada/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PracticaEvaluada
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexion = new SqlConnection("Data Source=.;Initial Catalog=VideoJuegos;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            string nombrej = txtN.Text;
+            string jugador = txtJ.Text;
+            string precio = txtP.Text;
+            string cantidad = txtC.Text;
+            string cadena = "insert into Juegos (Nombre,Jugador,Precio,Cantidad)values ('" + nombrej + "','" + jugador + "'," + precio + "," + cantidad + ")";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Los Datos se Guardaron correctamente");
+
+            int n = dgvJuegos.Rows.Add();
+            dgvJuegos.Rows[n].Cells[0].Value = txtN.Text;
+            dgvJuegos.Rows[n].Cells[1].Value = txtJ.Text;
+            dgvJuegos.Rows[n].Cells[2].Value = txtP.Text;
+            dgvJuegos.Rows[n].Cells[3].Value = txtC.Text;
+
+            txtN.Text = "";
+            txtJ.Text = "";
+            txtP.Text = "";
+            txtC.Text = "";
+
+
+            conexion.Close();
+
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            string idj = txtB.Text;
+            string cadena = "select Nombre,Jugador,Precio,Cantidad from Juegos where IDJ = " + idj;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                txtN.Text = registro["Nombre"].ToString();
+                txtJ.Text = registro["Jugador"].ToString();
+                txtP.Text = registro["Precio"].ToString();
+                txtC.Text = registro["Cantidad"].ToString();
+            }
+            else
+                MessageBox.Show("Ese Codigo No Existe!!!");
+            conexion.Close();
+
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            string idj = txtB.Text;
+            string nombrej = txtN.Text;
+            string jugador = txtJ.Text;
+            string precio = txtP.Text;
+            string cantidad = txtC.Text;
+            string cadena = "update Juegos set Nombre='"+nombrej+"',Jugador='"+jugador+"',Precio= "+precio+",Cantidad ="+cantidad+" where IDJ =" + idj;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int codigo;
+            codigo = comando.ExecuteNonQuery();
+            if (codigo == 1)
+            {
+                MessageBox.Show("Se Modificaron Los Datos!!!!");
+                txtB.Text = "";
+                txtN.Text = "";
+                txtJ.Text = "";
+                txtP.Text = "";
+                txtC.Text = "";
+
+            }
+            else
+                MessageBox.Show("No Existe Ningun ID con ese Caracter!!");
+            conexion.Close();
+            btnModificar.Enabled = false;
+
+
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            string id = txtB.Text;
+            string cadena = "delete from Juegos where IDJ =" + id;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int ID;
+            ID = comando.ExecuteNonQuery();
+            if (ID == 1)
+            {
+                txtN.Text = "";
+                txtJ.Text = "";
+                txtP.Text = "";
+                txtC.Text = "";
+
+                
+                MessageBox.Show("Se eliminaron correctamente los Datos!");
+
+
+            }
+            else
+                MessageBox.Show("No Existe Ningun ID Con esos Caracteres!");
+            conexion.Close();
+            btnEliminar.Enabled = false;
+
+        }
+
+        private void dgvJuegos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+ 
+            
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
